e57fde78996cbd90270514bc9424fdf2d2638323
MB406 <117555161+MB406@users.noreply.github.com>
2025-07-21 16:38:29 +0530

    Update check_limits.py
    Refactor battery check functions to reduce cyclomatic complexity

--- check_limits.py
+++ check_limits.py
@@ -1,18 +1,27 @@
 
+def temperature_is_ok(temperature):
+    if temperature < 0 or temperature > 45:
+        print('Temperature is out of range!')
+        return False
+    return True
+
+def soc_is_ok(soc):
+    if soc < 20 or soc > 80:
+        print('State of Charge is out of range!')
+        return False
+    return True
+
+def charge_rate_is_ok(charge_rate):
+    if charge_rate > 0.8:
+        print('Charge rate is out of range!')
+        return False
+    return True
+
 def battery_is_ok(temperature, soc, charge_rate):
-  if temperature < 0 or temperature > 45:
-    print('Temperature is out of range!')
-    return False
-  elif soc < 20 or soc > 80:
-    print('State of Charge is out of range!')
-    return False
-  elif charge_rate > 0.8:
-    print('Charge rate is out of range!')
-    return False
-
-  return True
-
-
-if __name__ == '__main__':
-  assert(battery_is_ok(25, 70, 0.7) is True)
-  assert(battery_is_ok(50, 85, 0) is False)
+    if not temperature_is_ok(temperature):
+        return False
+    if not soc_is_ok(soc):
+        return False
+    if not charge_rate_is_ok(charge_rate):
+        return False
+    return True
